0ead114776a9464d0a782899dfd3e85d92a4f395
blacknux <nahuel.claret@gmail.com>
2016-08-25 21:26:33 -0300

    New

---
+++ Claret.Nahuel/Clase04/Producto.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase04
+{
+    class Producto
+    {
+        private string codBarra;
+        private double precio;
+        private string nombre;
+
+        #region CONSTRUCTORES
+        /// <summary>
+        /// Constructro por default (Sin nombre,sin codigo de barras,sin precio)
+        /// </summary>
+        public Producto()
+        {
+            this.codBarra = "Sin codigo";
+            this.nombre = "Sin nombre";
+            this.precio = 0.0;
+        }
+
+        /// <summary>
+        /// Constructor solo con el precio del producto
+        /// </summary>
+        /// <param name="precio"></param>
+        public Producto(double precio):this( )
+        {
+            
+            //this.codBarra = "Sin codigo";
+            //this.nombre="Sin nombre";
+            
+            this.precio=precio;
+        }
+
+        /// <summary>
+        /// Inicializa con precio y codigo de barras
+        /// </summary>
+        /// <param name="codB"> Establece el codigo de barras del producto</param>
+        /// <param name="prec">Establece el precio de venta</param>
+        public Producto(string codB,double prec):this(prec)
+        {
+            this.codBarra = codB;
+            //this.precio = prec;
+            //this.nombre="Sin nombre";
+        }
+
+        /// <summary>
+        /// Inicializa el producto
+        /// </summary>
+        /// <param name="codB">Codigo de barras del producto</param>
+        /// <param name="prec">Precio de venta del producto</param>
+        /// <param name="name">Nombre del producto</param>
+        public Producto(string codB, double prec, string name)
+        {
+            this.codBarra = codB;
+            this.precio = prec;
+            this.nombre = name;
+        }
+
+        public Producto(double prec, string name): this(name)
+        {
+            this.precio = prec;
+        }
+
+        public Producto(string name)
+            : this()
+        {
+            this.nombre = name;
+        }
+
+        public Producto(double prec, string name, string codB)
+        {
+            this.codBarra = codB;
+            this.precio = prec;
+            this.nombre = name;
+        }
+
+        #endregion
+        /// <summary>
+        /// Muestra los valores que tiene cargado el producto.
+        /// </summary>
+        public void mostrar()
+        {
+            Console.WriteLine("El nombre es:{0}\nEl codigo de barras es:{1}\nEl precio es:{2}\n\n",this.nombre,this.codBarra,this.precio);
+        }
+    }
+}
---
+++ Claret.Nahuel/Clase04/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Producto P1 = new Producto();
+            Producto P2 = new Producto(10.56);
+            Producto P3 = new Producto( "Codigo1",0.65);
+            Producto P4 = new Producto("Codigo2", 5.23, "Producto 1");
+            //Producto P5 = new Producto(name:"reioerio", prec:25.65, codB:"Producto 6");
+            Producto P6 = new Producto("reioerio", 25.65, "Producto 6");
+            Producto P7 = new Producto(23.5, "prod 7");
+            Producto P9 = new Producto(23.5, "prod 7","asdsdsds");
+
+
+            P1.mostrar();
+            P2.mostrar();
+            P3.mostrar();
+            P4.mostrar();
+            //P5.mostrar();
+            P6.mostrar();
+
+           
+
+            Console.ReadKey();
+
+               
+
+        }
+    }
+}
