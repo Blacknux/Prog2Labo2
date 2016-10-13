16491f846e0253aa6b7a1b41f3470fde709bc144
unknown <nahuel.claret@gmail.com>
2016-10-12 21:57:47 -0300

    Clase 11

---
+++ Claret.Nahuel/Clase11/ColectoraDeNumeros.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase11
+{
+    public class ColectoraDeNumeros
+    {
+        protected List<Numero> _numeros;
+        public ETipoNumero TipoNumero { get; set; }
+        
+
+
+        private ColectoraDeNumeros()
+        {
+            this._numeros = new List<Numero>();
+        }
+        public ColectoraDeNumeros(ETipoNumero tipo)
+            : this()
+        {
+            this.TipoNumero = tipo;
+        }
+
+        public int SumaDeNumeros {
+            
+            get{
+                return (int)ColectoraDeNumeros.ObtenerResultado(this, ETipoResultado.Suma);
+
+
+                }
+            //set;
+        }
+        public int RestaDeNumeros
+        {
+
+            get
+            {
+
+                return (int)ColectoraDeNumeros.ObtenerResultado(this, ETipoResultado.Resta);
+
+
+
+            }
+           // set;
+        }
+        public int MultiplicacionDeNumeros
+        {
+
+            get
+            {
+
+                return (int)ColectoraDeNumeros.ObtenerResultado(this, ETipoResultado.Multiplicacion);
+            }
+        }
+        public double DivisionDeNumeros
+        {
+
+            get
+            {
+                
+               
+                return ColectoraDeNumeros.ObtenerResultado(this,ETipoResultado.Division);
+
+
+            }
+           // set;
+        }
+        public enum ETipoResultado{Suma, Resta, Multiplicacion, Division};
+        protected static double ObtenerResultado(ColectoraDeNumeros colectora, ETipoResultado tipoRes)
+        {
+            double retorno=0;
+            switch (tipoRes)
+            { 
+                case ETipoResultado.Division:
+                    
+                    for (int i = 1; i < colectora._numeros.Count; i++)
+                    {
+                        retorno /= colectora._numeros[0] / colectora._numeros[i];
+
+                    }
+                    
+                    break;
+                case ETipoResultado.Multiplicacion:
+
+                        for (int i = 1; i < colectora._numeros.Count; i++)
+                    {
+                        retorno *= colectora._numeros[0] * colectora._numeros[i];
+
+                    }   
+                    break;
+                case ETipoResultado.Resta:
+
+
+                    for (int i = 1; i < colectora._numeros.Count; i++)
+                    {
+                        retorno -= colectora._numeros[0] - colectora._numeros[i];
+
+                    }
+                    break;
+                case ETipoResultado.Suma:
+                    for (int i = 1; i < colectora._numeros.Count; i++)
+                    {
+                        retorno += colectora._numeros[0] + colectora._numeros[i];
+
+                    }
+                    break;
+                  
+            }
+            return retorno;
+        }
+
+
+
+        public static ColectoraDeNumeros operator +(ColectoraDeNumeros colectora, Numero numero)
+        {
+            //bool flag = false;
+            //foreach (Numero item in colectora._numeros)
+            //{
+            //    if (item == numero)
+            //    {
+            //        flag = true;
+            //        break;
+            //    }
+                
+            //}
+            //if (!flag)
+            //{
+                colectora._numeros.Add(numero);
+            //}
+
+            return colectora;
+            
+            
+        }
+
+       
+
+        public static ColectoraDeNumeros operator -(ColectoraDeNumeros colectora, Numero numero)
+        {
+            
+            //foreach (Numero item in colectora._numeros)
+            //{
+            //    if (item == numero)
+            //    {
+                    colectora._numeros.Remove(numero);
+            //        break;
+            //    }
+
+            //}
+            
+            return colectora;
+           }
+
+        public override string ToString()
+        {
+            StringBuilder retorno = new StringBuilder();
+            retorno.AppendLine("El tipo es: " + this.TipoNumero);
+            for (int i = 0; i < this._numeros.Count; i++)
+            {
+                retorno.AppendLine(this._numeros[i].Lectura.ToString());
+                
+
+            }
+
+            return retorno.ToString();
+            
+        }
+
+
+    }
+}
---
+++ Claret.Nahuel/Clase11/Numero.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase11
+{
+    public class Numero
+    {
+        protected int _numero;
+
+        public int Lectura { get { return this._numero;} }
+
+        public Numero(int numero)
+        {
+            if (numero < 0)
+            {
+                throw new Exception("negativos");
+            }
+            else
+            {
+                this._numero = numero;
+            }
+        }
+
+        public static int operator +(Numero n1, Numero n2)
+        {
+            return n1._numero + n2._numero;
+        }
+        public static int operator -(Numero n1, Numero n2)
+        {
+            return n1._numero - n2._numero;
+        }
+        public static double operator /(Numero n1, Numero n2)
+        {
+            return n1._numero /  n2._numero;
+        }
+        public static int operator *(Numero n1, Numero n2)
+        {
+            return n1._numero * n2._numero;
+        }
+
+
+    }
+}
